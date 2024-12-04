00ce6fd184c14a005d0683c1c7ab5f025f9b5ea6
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2024-12-03 23:11:05 -0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "2d physics engine", NULL, NULL);
+
+	if (window == NULL) {
+
+		std::cout << "Failed to create GLFW window";
+
+		glfwTerminate();
+
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 600);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
